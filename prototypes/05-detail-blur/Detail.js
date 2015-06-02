dc3fafa83be9e0c5b44d75288f70674d59fdb77c
Abe Yang <abeyang@gmail.com>
2015-06-02 12:15:37 -0700

    Updated Detail Pane: colors + text

--- prototypes/05-detail-blur/Detail.js
+++ prototypes/05-detail-blur/Detail.js
@@ -6,36 +6,49 @@ app.controller('DetailController', function($scope, ui) {
 		{
 			uid: 'chef',
 			name: 'Chef',
+			year: '2014',
 			director: 'Jon Favreau'
 		},
 		{
 			uid: 'ironman',
 			name: 'Iron Man 3',
+			year: '2013',
 			director: 'Shane Black'
 		},
 		{
 			uid: 'holmes',
 			name: 'Sherlock Holmes',
+			year: '2009',
 			director: 'Guy Ritchie'
 		},
 		{
 			uid: 'wonderboys',
 			name: 'Wonder Boys',
+			year: '2000',
 			director: 'Curtis Hanson'
 		},
 		{
 			uid: 'dreams',
 			name: 'In Dreams',
+			year: '1999',
 			director: 'Neil Jordan'
+		},
+		{
+			uid: 'creation',
+			name: 'Creation',
+			year: '2010',
+			director: 'Jon Amiel'
 		},
 		{
 			uid: 'minorityreport',
 			name: 'Minority Report',
+			year: '2002',
 			director: 'Steven Spielberg'
 		},
 		{
 			uid: 'lastsamurai',
 			name: 'Last Samurai',
+			year: '2003',
 			director: 'Edward Zwick'
 		}
 	];
