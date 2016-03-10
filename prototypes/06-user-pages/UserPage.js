ec7056a9a7b765b9a8dd22fed27ab8e8e3c5f4dd
Abe Yang <abeyang@gmail.com>
2016-03-09 18:13:20 -0800

    updated types of IAs

--- prototypes/06-user-pages/UserPage.js
+++ prototypes/06-user-pages/UserPage.js
@@ -3,13 +3,22 @@ app.controller('UserPageController', function($scope, ui) {
 	$scope.ui = ui;
 
 	// CHANGE USERNAME TO GET SPECIFIC USER
-	//var username = 'GuiltyDolphin';
+	var username = 'GuiltyDolphin';
 	// var username = 'AnthonyNeace';
-	var username = 'riqpe';
+	// var username = 'riqpe';
 
 	$scope.user = eval(username);
+
+	// developed IAs -- using http://underscorejs.org/
 	$scope.ias = _.filter(ias, function(ia) { 
   		return _.some(ia.developer, function(d) { return d.name == username});
+	});
+
+	// maintained IAs
+	$scope.ias_maintained = _.filter(ias, function(ia) { 
+		var found = false;  		
+		if (ia.maintainer) { if (ia.maintainer.github == username) found = true; }
+		return found;
 	});
 
 });
