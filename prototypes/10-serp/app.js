94dd060fbf1a811d98aac531c854facd84ad95df
Abe Yang <abeyang@gmail.com>
2016-11-08 11:39:52 -0800

    10. added 03

--- prototypes/10-serp/app.js
+++ prototypes/10-serp/app.js
@@ -12,7 +12,23 @@ var app = new Vue({
 // serp
 Vue.component('serp-result', {
 	props: ['serp'],
-	template: '#serp-result'
+	template: '#serp-result',
+	methods: {
+		// a computed getter
+		getLink: function () {
+			// `this` points to the vm instance
+			return 'http://' + this.serp.url;
+		},
+		gotoLink: function() {
+			location.assign(this.getLink());
+		},
+		getDate: function() {
+			var date = this.serp.date;
+			if (date) {
+				return moment(date, "YYYYMMDD").format("MMM DD, YYYY");
+			}
+		}
+	}
 });
 
 var serp = new Vue({
@@ -41,13 +57,35 @@ var serp = new Vue({
 				title: '<b>Tycho</b> - Dive {Full Album} - YouTube',
 				snippet: '<b>Tycho</b> - Dive {Full Album} itskerim. Subscribe Subscribed Unsubscribe 17,946 17K. ... <b>Tycho</b> - Past is Prologue {FULL ALBUM} - Duration: 1:03:29.',
 				url: 'youtube.com/watch?v=Z6ih1aKeETk',
-				favicon: 'https://duckduckgo.com/assets/icons/favicons/youtube.2x.png'
+				favicon: 'https://duckduckgo.com/assets/icons/favicons/youtube.2x.png',
+				date: '20121121'
+			},
+			{
+				title: '<b>Tycho</b> - Awake [Full Album] - YouTube',
+				snippet: '<b>Tycho\'s</b> Brand New Album "Awake" released March 18, 2014. Tracklist: 1. Awake 2. Montana 3. L 4. Dye 5',
+				url: 'youtube.com/watch?v=qEI1_oGPQr0',
+				favicon: 'https://duckduckgo.com/assets/icons/favicons/youtube.2x.png',
+				date: '201403¢21'
 			},
 			{
 				title: '<b>Tycho</b> — Ghostly International',
 				snippet: 'As <b>Tycho</b>, Scott Hansen blends swirling melodies into vaguely triumphant arcs that crisscross between stuttering beats and vocal samples, creating rolling sonic',
 				url: 'ghostly.com/artists/tycho',
 				favicon: 'https://icons.duckduckgo.com/ip2/ghostly.com.ico'
+			},
+			{
+				title: 'ISO50 Blog – The Blog of Scott Hansen (<b>Tycho</b> / ISO50) » The blog of ...',
+				snippet: 'Now that the new <b>Tycho</b> album — Epoch — is out I wanted to write a little about the meaning and origin of the artwork. I worked as a graphic',
+				url: 'blog.iso50.com/',
+				favicon: 'https://icons.duckduckgo.com/ip2/blog.iso50.com.ico',
+				date: '20161027'
+			},
+			{
+				title: 'Chandra :: Photo Album :: <b>Tycho\'s</b> Supernova Remnant :: May 12...',
+				snippet: 'For the first time, a movie has been made of the evolution of <b>Tycho\'s</b> supernova remnant. This sequence includes X-rays observations from Chandra spaced out over a',
+				url: 'chandra.harvard.edu/photo/2016/tycho/',
+				favicon: 'https://icons.duckduckgo.com/ip2/chandra.harvard.edu.ico',
+				date: '20160512'
 			}
 		]
 	}
--- prototypes/10-serp/index.md
+++ prototypes/10-serp/index.md
@@ -5,3 +5,9 @@ Trying to mimic the current SERP pages.
 
 ## 02. [No Favicon / Colored Footer](02-no_favicon-color_footer/)
 Playing around with Thom's version. Simplified on-hover.
+
+## 03. [Grayscaled Favicon / Colored Footer](03-favicon-color_footer/)
+
+- put the favicon back in, but simplified it. 
+- added dates
+- removed "more results"
