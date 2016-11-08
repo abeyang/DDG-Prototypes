7a7449a1e1194ea8493171ff3a4539e5b1d39b6d
Abe Yang <abeyang@gmail.com>
2016-11-07 16:21:41 -0800

    10: update to 02 and global files

--- prototypes/10-serp/app.js
+++ prototypes/10-serp/app.js
@@ -32,11 +32,23 @@ var serp = new Vue({
 				favicon: 'https://duckduckgo.com/assets/icons/favicons/wikipedia.2x.png'
 			},
 			{
-				title: '<b>Tycho</b> — Free listening, videos, concerts, stats and photos at',
+				title: '<b>Tycho</b> — Free listening, videos, concerts, stats and photos at ...',
 				snippet: 'Scott Hansen (born 1976/1977), professionally known as <b>Tycho</b>, is an American ambient music artist and producer, who is known as ISO50 for his photographic and design',
 				url: 'last.fm/music/Tycho',
 				favicon: 'https://icons.duckduckgo.com/ip2/www.last.fm.ico'
 			},
+			{
+				title: '<b>Tycho</b> - Dive {Full Album} - YouTube',
+				snippet: '<b>Tycho</b> - Dive {Full Album} itskerim. Subscribe Subscribed Unsubscribe 17,946 17K. ... <b>Tycho</b> - Past is Prologue {FULL ALBUM} - Duration: 1:03:29.',
+				url: 'youtube.com/watch?v=Z6ih1aKeETk',
+				favicon: 'https://duckduckgo.com/assets/icons/favicons/youtube.2x.png'
+			},
+			{
+				title: '<b>Tycho</b> — Ghostly International',
+				snippet: 'As <b>Tycho</b>, Scott Hansen blends swirling melodies into vaguely triumphant arcs that crisscross between stuttering beats and vocal samples, creating rolling sonic',
+				url: 'ghostly.com/artists/tycho',
+				favicon: 'https://icons.duckduckgo.com/ip2/ghostly.com.ico'
+			}
 		]
 	}
 });
