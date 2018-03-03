89e87b6c5a08112e32b9ed34da090d524da3ebb2
Abe Yang <abeyang@gmail.com>
2018-03-02 16:30:17 -0800

    12:pgc
    rudimentary 4 criteria now, with scores.

--- prototypes/12-privacy-grade-calc/app.js
+++ prototypes/12-privacy-grade-calc/app.js
@@ -21,17 +21,6 @@ var pg = new Vue({
 				id: 'connection',
 				title: 'Type of Connection',
 				snippet: 'An encrypted connection prevents eavesdropping of any personal information you send to a website.',
-				type: 'radio',
-				options: [
-					{
-						title: 'Encrypted',
-						points: 0
-					},
-					{
-						title: 'Unencrypted',
-						points: -1
-					}
-				],
 				toggle: false,
 				desc: 'You may realize that hidden trackers lurk on many sites you visit. What you may not realize, though, is 76 percent of websites now contain hidden Google trackers, 24 percent have hidden Facebook trackers, and several other major tracker networks are at 10%+.Because these major tracker networks are on so many pages…',
 				points: 0
@@ -40,17 +29,26 @@ var pg = new Vue({
 				id: 'trackers',
 				title: 'Tracker Networks',
 				snippet: 'Tracker networks aggregate your web history into a data profile about you.',
-				type: 'scale',
-				options: [{
-					min: 0,
-					max: 20,
-					points: -4
-				}],
 				toggle: false,
 				desc: 'You may realize that hidden trackers lurk on many sites you visit. What you may not realize, though, is 76 percent of websites now contain hidden Google trackers, 24 percent have hidden Facebook trackers, and several other major tracker networks are at 10%+.Because these major tracker networks are on so many pages…',
 				points: 0
-			}
-
+			},
+			{
+				id: 'majornetworks',
+				title: 'Major Tracker Networks',
+				snippet: 'These are more harmful because they can track and target you across more of the internet.',
+				toggle: false,
+				desc: 'You may realize that hidden trackers lurk on many sites you visit. What you may not realize, though, is 76 percent of websites now contain hidden Google trackers, 24 percent have hidden Facebook trackers, and several other major tracker networks are at 10%+.Because these major tracker networks are on so many pages…',
+				points: 0
+			},
+			{
+				id: 'ismajornetwork',
+				title: 'Is this a Major Tracker Network?',
+				snippet: 'Is this site itself a major tracker network?',
+				toggle: false,
+				desc: 'You may realize that hidden trackers lurk on many sites you visit. What you may not realize, though, is 76 percent of websites now contain hidden Google trackers, 24 percent have hidden Facebook trackers, and several other major tracker networks are at 10%+.Because these major tracker networks are on so many pages…',
+				points: 0
+			},
 		],
 		score: 0,
 		grade: 'A',
@@ -69,7 +67,16 @@ var pg = new Vue({
 		presets: {
 			'default': {
 				isencrypted: false, 
-				trackers: 10
+				trackers: 10,
+				major: {
+					google: true,
+					facebook: true,
+					twitter: false,
+					amazon: false,
+					appnexus: true,
+					oracle: false
+				},
+				isthismajor: true
 			}
 		}
 	},
@@ -91,6 +98,8 @@ var pg = new Vue({
 		calculateScore: function() {
 			this.rows[0].points = (this.x.isencrypted) ? 0 : -1;
 			this.rows[1].points = -Math.floor(this.x.trackers/4);
+			this.rows[2].points = -_.filter(this.x.major, function(val, key) { return val==true}).length;
+			this.rows[3].points = (this.x.isthismajor) ? -1 : 0;
 
 			var total = 0;
 			var point_arr = _.pluck(this.rows, 'points');
